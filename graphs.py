722436d7b9698318bb13c5dde679a768846f908a
Lucas Paim <lucaspaim97@hotmail.com>
2019-04-03 01:19:49 -0300

    implementando: adicao de vertices e arestas, exibicao do grafo e da matriz

--- graphs.py
+++ graphs.py
@@ -6,6 +6,12 @@ import argparse
 
 # Módulo de fora
 import networkx as nx
+import matplotlib.pyplot as plt 
+
+
+
+
+G=nx.Graph() #grafo
 
 option = -1
 menu = """
@@ -13,9 +19,9 @@ menu = """
 
     Escolha uma das opções a seguir:
 
-    1 - Criar um Grafo
+    1 - Exibir Grafo
     2 - Adicionar vértices
-    3 - Adicionar arestas:
+    3 - Adicionar arestas
     4 - Imprimir Matriz
     5 - Imprimir Dicionário de Grafos
     ...
@@ -30,21 +36,50 @@ def optionAction(option):
 
     if option == 1: # Cria um grafo vazio
         
-        G=nx.Graph()
+        
+        nx.draw(G, with_labels=True)
+        #plt.savefig("teste.png")  #este comando cria um arquivo png do grafo
+        plt.show()
 
-        print("Grafo criado:")
-        print( "Vértices: {0}".format( G.nodes() ) )
-        print( "Arestas: {0}".format(G.edges()))
+        #G=nx.Graph() #grafo
+        #print("Grafo criado:")
+        #print( "Vértices: {0}".format( G.nodes() ) )
+        #print( "Arestas: {0}".format(G.edges()))
+
         input('Aperte enter para continuar...')
 
     elif option == 2: # adiciona vértices
         # verifica se existe um grafo, para assim poder adicionar os vértices
+        vertices= []
+        print("Insira a quantidade de vértices a serem inseridos")
+        i= int(input()) #transformar a string de input() para int é diferente nas versoes do python 2 e 3
+        cont= 0
+        while cont < i:
+            print("Insira o vértice %d" % (cont+1)) #a funcao print() pode ser diferente a depender da versao do python no PC
+            vertices.append(input())
+            cont+=1
+        for x in vertices:
+            G.add_node(x)
         pass
     elif option == 3: # adiciona arestas
         # verifica se existe um grafo com vértices, para assim poder adicionar as arestas
+        print("Insira a quantiade de arestas a serem inseridas")
+        i= int(input())
+        cont= 0
+        while cont < i:
+            print("--- Aresta %d ---" % (cont+1))
+            print("Insira o vértice de origem")
+            x= input()
+            print("Insira o vértice de destino")
+            y= input()
+            G.add_edge(x, y)
+            cont+=1
         pass
     elif option == 4: # imprime matriz
         # verifica se existe um grafo e imprime uma matriz representativa
+        M=nx.to_numpy_matrix(G)
+        print(M)
+        input('Aperte enter para continuar...')
         pass
     elif option == 5: # imprime dicionário de grafos
         # verifica se exuste um grafo e imprime um dicionário representativo
